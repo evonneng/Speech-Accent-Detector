47c2b71af01269ef17952d8fc28a47e5b35c5282
cynthwjwu <cynthwjwu@gmail.com>
2018-12-11 23:23:07 -0600

    converted files now sorted by language

--- convert_to_wav.sh
+++ convert_to_wav.sh
@@ -1,6 +1,25 @@
-for file in $(ls); do
+#create directory to hold all wav files
+if [ ! -d "recordings_wav" ]; then
+	mkdir "recordings_wav";
+fi
+
+#convert mp3 to wav
+for file in $(ls recordings); do
     if [ ${file: -4} == ".mp3" ]
     then
-        ffmpeg -i $file ../recordings_wav/${file%.*}.wav
+    	#parse file for language name
+    	dir_name="${file%'.mp3'}"
+    	dir_name="${dir_name//[[:digit:]]/}"
+
+    	#create directory for language
+    	if [ ! -d "./recordings_wav/${dir_name}" ]; then
+			mkdir ./recordings_wav/${dir_name};
+		fi
+
+		#convert and store wav in recordings_wav/name_of_language/
+        if [ ! -e "./recordings_wav/${dir_name}/${file%.*}.wav" ]; then
+        	ffmpeg -i recordings/$file ./recordings_wav/${dir_name}/${file%.*}.wav;
+        fi
     fi
 done
+
