067821ce7a4d0f8048b87e03b79d5cf1ab2ce64a
evonneng <evonne_ng@utexas.edu>
2018-12-17 11:39:12 -0600

    added num languages option

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import numpy as np
 dirname = os.path.dirname(os.path.abspath(__file__))
 model = Model()
 model.load_state_dict(torch.load(os.path.join(dirname, 'model.th'), map_location=lambda storage, loc: storage))
-languages = ['arabic', 'english', 'french', 'spanish', 'russian']
+languages = ['english', 'spanish', 'french', 'arabic', 'russian']
 
 def get_probs(mfcc):
 	logits = model(torch.from_numpy(mfcc).float().unsqueeze(0))
@@ -15,7 +15,7 @@ def get_probs(mfcc):
 	logits = np.mean(np.squeeze(logits), axis=0)
 	return 1. / (1. + np.exp(-logits))
 
-def evaluate(filename):
+def evaluate(filename, numlang):
 	if filename is not None:
 		print('Loading in WAV file')
 		wav = setup.get_wav(filename)
@@ -31,8 +31,8 @@ def evaluate(filename):
 		print('Starting')
 		total_correct = 0
 		total = 0
-		for idx, language in enumerate(languages):
-			language_dir = os.path.join(dirname, 'recordings_wav', language)
+		for idx in range(numlang):
+			language_dir = os.path.join(dirname, 'recordings_wav', languages[i])
 			files = os.listdir(language_dir)
 			language_correct = 0
 			language_total = 0
@@ -46,11 +46,12 @@ def evaluate(filename):
 				if prediction == idx:
 					language_correct += 1
 					total_correct += 1
-			print(language + ': %d/%d (%4f)' % (language_correct, language_total, language_correct / language_total))
+			print(languages[i] + ': %d/%d (%4f)' % (language_correct, language_total, language_correct / language_total))
 		print('Overall: %d/%d (%4f)' % (total_correct, total, total_correct / total))
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-f', '--filename')
+	parser.add_argument('-l', '--numlang')
 	args = parser.parse_args()
-	evaluate(args.filename)
+	evaluate(args.filename, args.numlang)
